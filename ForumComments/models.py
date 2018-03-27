f6e01d4989f639918a63766184747f79136c782a
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-03-27 13:41:33 +0530

    added timestamp and last updated

--- ForumComments/models.py
+++ ForumComments/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Comments(models.Model):
+
     id = models.BigAutoField(primary_key=True)
     comment = models.CharField(max_length= 255)
-    replies = models.ForeignKey('self',on_delete=models.CASCADE)
-
-    
+    parent_comment = models.ForeignKey('self',on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
